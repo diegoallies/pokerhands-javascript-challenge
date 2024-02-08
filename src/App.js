f2a97cb0adb30f75d1021e7548e0934425b8b92f
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-02-08 03:25:54 +0200

    commit changes to backup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css';
+import './App.scss';
 import { useDeck } from "./hooks/useDeck";
 import { useHandRanker } from "./hooks/useHandRanker";
 
